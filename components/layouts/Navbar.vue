c9692916eae0245fd1fb913c92c6b0aa1e3e429c
Tomoyuki Shimizu <smz2develop@gmail.com>
2020-09-30 03:39:18 +0900

    Add logo brands svg

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -20,7 +20,7 @@
       </div>
       <div
         :class="isOpen ? 'block' : 'hidden'"
-        class="w-full flex-grow lg:flex lg:items-center lg:w-auto"
+        class="w-full lg:flex lg:items-center lg:w-auto"
       >
         <div v-for="(link, index) in links" :key="index">
           <nuxt-link
@@ -30,6 +30,44 @@
             {{ link.name }}
           </nuxt-link>
         </div>
+      </div>
+      <div class="hidden lg:flex">
+        <svg
+          width="20%"
+          height="20%"
+          viewBox="0 0 449 232"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          xml:space="preserve"
+          xmlns:serif="http://www.serif.com/"
+          style="
+            fill-rule: evenodd;
+            clip-rule: evenodd;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          "
+        >
+          <rect
+            x="1.5"
+            y="75.84"
+            width="435.06"
+            height="64.04"
+            style="fill: none; stroke: rgb(101, 101, 101); stroke-width: 3px"
+          />
+          <path
+            d="M123.62,170.59C123.62,198.07 100,231.99 71.43,231.99C42.86,231.99 20.09,198.07 20.09,170.59L20.09,0L123.62,0L123.62,170.59Z"
+            style="fill: rgb(101, 101, 101)"
+          />
+          <path
+            d="M170.86,0L309.37,0L170.86,231.99L170.86,0Z"
+            style="fill: rgb(101, 101, 101)"
+          />
+          <path
+            d="M310,0L448.51,0L310,231.99L310,0Z"
+            style="fill: rgb(101, 101, 101)"
+          />
+        </svg>
       </div>
     </nav>
   </div>
